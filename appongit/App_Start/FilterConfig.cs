d430d2640d3b8cdd1bcb800a96709fd82dfa1104
Vinme Kako <kako2015.4@gmail.com>
2017-12-05 23:52:56 +0400

    Add project files.

---
+++ appongit/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace appongit
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ appongit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="appongit.MvcApplication" Language="C#" %>
